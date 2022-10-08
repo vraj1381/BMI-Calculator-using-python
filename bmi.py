36f6a819e86287443bc3270828618d19e7044661
Khilan Patel <41852510+coderMikhael@users.noreply.github.com>
2022-10-08 19:44:55 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -3,9 +3,11 @@ height = float(input());
 x = weight/float(height*height);
 if x < 18.5:
     print('Underweight')
-if x>=18.5 and x<25:
+else if x>=18.5 and x<25:
     print("Normal")
-if x >= 25 and x < 30:
+else if x >= 25 and x < 30:
    print('Overweight')
-if x >= 30:
+else x >= 30:
    print('Obesity')
+#use of if-else ladder
+#reducing execution time
